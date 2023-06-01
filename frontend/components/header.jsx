a2c0a3108614329c02f6135a14a9f54571a6964e
qj coder <qjcoder@gmail.com>
2023-06-01 16:22:33 +0500

    mobilemenucompletd

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Wrapper from "./wrapper";
+import Link from "next/link";
+import Menu from "./menu";
+
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BsCart } from "react-icons/bs";
+import { BiMenuAltRight } from "react-icons/bi";
+import { VscChromeClose } from "react-icons/vsc";
+import MobileMenu from "./mobilemenu";
 
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
-  const [showCatMenu, setCatMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
   const [show, setShow] = useState("translate-y-0");
   const [lastScrollY, setLastScrollY] = useState(0);
 
@@ -15,7 +23,64 @@ const Header = () => {
       z-20 sticky top-0 
       transition-transform duration-300 ${show}`}
     >
-      <Wrapper></Wrapper>
+      <Wrapper className="h-[60px] flex justify-between items-center">
+        <Link href={"/"}>
+          <img src="/logo.svg" className="w-[40px] md:w-[80px]" />
+        </Link>
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+        {mobileMenu && (
+          <MobileMenu
+            showCatMenu={showCatMenu}
+            setShowCatMenu={setShowCatMenu}
+            setMobileMenu={setMobileMenu}
+          />
+        )}
+        <div className="flex items-center gap-2 text-black ">
+          {/*Icon start*/}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[19px] md:text-[24px] " />
+            <div
+              className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] 
+            rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white
+            text-[10px] md:text-[12px] flex justify-center items-center px-[2px]
+            md:px-[5px] "
+            >
+              5
+            </div>
+          </div>
+
+          {/*Icon end*/}
+
+          {/*Icon start*/}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <BsCart className="text-[15px] md:text-[20px] " />
+            <div
+              className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] 
+            rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white
+            text-[10px] md:text-[12px] flex justify-center items-center px-[2px]
+            md:px-[5px]  "
+            >
+              5
+            </div>
+          </div>
+          {/*Icon end*/}
+          {/*Mobile Icon start*/}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[16px] "
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[20px] "
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
+          </div>
+          {/*Mobile Icon end*/}
+        </div>
+      </Wrapper>
     </header>
   );
 };
